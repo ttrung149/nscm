dc996ad7260df359c7c5bd6895bc479d3704a17b
Trung Truong <ttruong91498@gmail.com>
2020-04-29 10:33:27 -0400

    Add float type

--- nscm.cpp
+++ nscm.cpp
@@ -11,20 +11,24 @@
 #define DEBUG
 
 #include <iostream>
-#include <inttypes.h>
 #include <memory>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <tuple>
 #include <unordered_map>
-#include <functional>
+#include <inttypes.h>
+#include <math.h>
 
 /*============================================================================
- *  Enums and constatn
+ *  Enums and constants
  *===========================================================================*/
-enum class ExpType { LIT, INT, SYMBOL, PROC, PRIM };
-enum class PrimType { ADD, SUB, MUL, DIV, MOD, IF, WHILE };
+enum class ExpType { LIT, INT, FLOAT, SYMBOL, PROC, PRIM };
+enum class PrimType { 
+    ADD, SUB, MUL, DIV, MOD, GT, LT, GE, LE,        // Arithmetic operations
+    IF, WHILE, DEFINE, SET,                         // Control flow
+    IS_NUM, IS_SYMBOL, IS_PROC                      // Type check
+};
 enum class LitType { TRUE, FALSE, NIL };
 
 /* Forward-declartion of `Env` class */
@@ -38,14 +42,16 @@ private:
     ExpType type;
     union {
         int64_t ival;
+        double fval;
         std::string symbol;
         LitType lit;
         std::tuple<PrimType, std::vector<Expr*> *, Env*> prim; 
-        std::tuple<std::vector<Expr*> *, Expr*, Env*> proc;
+        std::tuple<Expr*, Expr*, Env*> proc;
     };
 
 public:
     Expr(int64_t i)      { type = ExpType::INT; ival = i; };
+    Expr(double f)       { type = ExpType::FLOAT; fval = f; };
     Expr(std::string s)  { type = ExpType::SYMBOL; symbol = s; };
     Expr(LitType l)      { type = ExpType::LIT; lit = l; };
 
@@ -53,7 +59,7 @@ public:
         type = ExpType::PRIM;
         prim = std::make_tuple(t, args, env);
     };
-    Expr(std::vector<Expr*> *params, Expr *body, Env *env) {
+    Expr(Expr *params, Expr *body, Env *env) {
         type = ExpType::PROC;
         proc = std::make_tuple(params, body, env);
     };
@@ -87,6 +93,9 @@ Expr* Expr::eval_prim(Env *e) {
             if (cond->type == ExpType::INT && cond->ival > 0)
                 return args[1]->eval(e);
 
+            if (cond->type == ExpType::FLOAT && cond->fval > 0.0)
+                return args[1]->eval(e);
+
             return args[2]->eval(e);
         }
         /* While statement */
@@ -103,41 +112,77 @@ Expr* Expr::eval_prim(Env *e) {
         /*======================= Arith operations =======================*/
         /* Integer addition */
         case PrimType::ADD: {
-            int64_t s = 0;
+            double s = 0.0;
             for (const auto &arg : args) {
-                s += (arg->eval(e))->ival;
+                Expr *exp = arg->eval(e);
+                if (exp->type == ExpType::INT)          s += exp->ival;
+                else if (exp->type == ExpType::FLOAT)   s += exp->fval;
+                else throw "Invalid args type for '+'";
             }
-            return new Expr(s);
+            if (std::floor(s) == s) return new Expr(int64_t(s));
+            else return new Expr(s);
         }
         /* Integer subtraction */
         case PrimType::SUB: {
             if (args.size() != 2) throw "Invalid num args for '-'";
-            int64_t d = args[0]->eval(e)->ival - args[1]->eval(e)->ival;
-            return new Expr(d);
+            Expr *e1 = args[0]->eval(e);
+            Expr *e2 = args[1]->eval(e);
+
+            if (e1->type == ExpType::INT && e2->type == ExpType::INT)
+                return new Expr(int64_t(e1->ival - e2->ival)); 
+            else if (e1->type == ExpType::FLOAT && e2->type == ExpType::INT)
+                return new Expr(e1->fval - e2->ival);
+            else if (e1->type == ExpType::INT && e2->type == ExpType::FLOAT)
+                return new Expr(e1->ival - e2->fval);
+            else if (e1->type == ExpType::FLOAT && e2->type == ExpType::FLOAT)
+                return new Expr(e1->fval - e2->fval);
+            else throw "Invalid args type for '-'";
         }
         /* Integer multiplication */
         case PrimType::MUL: {
-            int64_t p = 1;
+            double p = 1.0;
             for (const auto &arg : args) {
-                p *= arg->eval(e)->ival;
+                Expr *exp = arg->eval(e);
+                if (exp->type == ExpType::INT)          p *= exp->ival;
+                else if (exp->type == ExpType::FLOAT)   p *= exp->fval;
+                else throw "Invalid args type for '*'";
             }
-            return new Expr(p);
+            if (std::floor(p) == p) return new Expr(int64_t(p));
+            else return new Expr(p);
         }
         /* Integer division */
         case PrimType::DIV: {
             if (args.size() != 2) throw "Invalid num args for '/'";
-            int64_t den = (args[1]->eval(e))->ival;
-            if (den == 0) throw "Division by zero";
-            int64_t num = (args[0]->eval(e))->ival;
-            return new Expr(num / den);
+            Expr *e1 = args[0]->eval(e);
+            Expr *e2 = args[1]->eval(e);
+
+            if ((e2->type == ExpType::INT && e2->ival == 0) ||
+                (e2->type == ExpType::FLOAT && e2->fval == 0)) 
+                throw "Division by zero";
+
+            if (e1->type == ExpType::INT && e2->type == ExpType::INT)
+                return new Expr(int64_t(e1->ival / e2->ival)); 
+            else if (e1->type == ExpType::FLOAT && e2->type == ExpType::INT)
+                return new Expr(e1->fval / e2->ival);
+            else if (e1->type == ExpType::INT && e2->type == ExpType::FLOAT)
+                return new Expr(e1->ival / e2->fval);
+            else if (e1->type == ExpType::FLOAT && e2->type == ExpType::FLOAT)
+                return new Expr(e1->fval / e2->fval);
+            else throw "Invalid args type for '/'";
         }
         /* Integer modulo */
         case PrimType::MOD: {
             if (args.size() != 2) throw "Invalid num args for 'modulo'";
-            int64_t den = (args[1]->eval(e))->ival;
-            if (den == 0) throw "Division by zero";
-            int64_t num = (args[0]->eval(e))->ival;
-            return new Expr(num % den);
+            Expr *e1 = args[0]->eval(e);
+            Expr *e2 = args[1]->eval(e);
+                    
+            if ((e2->type == ExpType::INT && e2->ival == 0) ||
+                (e2->type == ExpType::FLOAT && e2->fval == 0)) 
+                throw "Division by zero";
+            
+            if (e1->type == ExpType::INT && e2->type == ExpType::INT)
+                return new Expr(e1->ival % e2->ival);
+            else throw "Invalid args type for 'modulo'";
         }
         /*======================= Invalid primative =======================*/
         default: throw "Invalid primitive'";
@@ -146,12 +191,13 @@ Expr* Expr::eval_prim(Env *e) {
 
 /**
  * Evaluate expression
- * @param e pointer to env
+ * @param env pointer to env
  * @returns Pointer to evaluated expression 
  */
 Expr* Expr::eval(Env *env) {
     switch (type) {
         case ExpType::INT:     { return this; }
+        case ExpType::FLOAT:   { return this; }
         case ExpType::LIT:     { return this; }
         case ExpType::PRIM:    { return eval_prim(env); }
         default:               { return nullptr; }
@@ -160,13 +206,22 @@ Expr* Expr::eval(Env *env) {
 
 /**
  * Print expression to stdout
- * @returns vood
+ * @returns void
  */
 void Expr::print_to_console(void) {
     switch (type) {
         case ExpType::INT:     { std::cout << ival << "\n"; break; }
+        case ExpType::FLOAT:   { std::cout << fval << "\n"; break; }
         case ExpType::PROC:    { std::cout << "<procedure>\n"; break; }
         case ExpType::PRIM:    { std::cout << "<primitive>\n"; break; }
+        case ExpType::LIT:     {
+            switch (lit) {
+                case LitType::TRUE:     std::cout<< "#t\n"; break;
+                case LitType::FALSE:    std::cout<< "#f\n"; break;
+                case LitType::NIL:      std::cout<< "()\n"; break;
+                default: break;
+            }
+        }
         default:               break;
     }
 }
@@ -194,31 +249,36 @@ public:
     };
 };
 
+/*============================================================================
+ *  Main driver
+ *===========================================================================*/
 int main(int argc, char*argv[]) {
     (void) argc;
     (void) argv;
 
-    Expr* a = new Expr(LitType::TRUE);
-    Expr* b = new Expr(int64_t(5));
-    Expr* c = new Expr(int64_t(10));
-    Expr* d = new Expr(int64_t(10.5));
-    std::vector<Expr*> x = {a, b, c};
-
-    Expr *e = new Expr(PrimType::IF, &x, nullptr);
+    // Expr* lit_true = new Expr(LitType::TRUE);
+    Expr* x = new Expr(int64_t(10));
+    Expr* y = new Expr(int64_t(2));
+    Expr* z = new Expr(9.5);
+    std::vector<Expr*> mul_args = {x, y};
+    Expr *mul_x_y = new Expr(PrimType::MUL, &mul_args, nullptr);
+    
+    std::vector<Expr*> add_z_args = {mul_x_y, z};
+    Expr *res = new Expr(PrimType::ADD, &add_z_args, nullptr);
 
     try {
-        (e->eval(nullptr))->print_to_console();
+        (res->eval(nullptr))->print_to_console();
     }
     catch (const char* e) {
         std::cerr << "ERR: " << e << "\n";
         exit(EXIT_FAILURE);
     }
 
-    delete a;
-    delete b;
-    delete c;
-    delete d;
-    delete e;
+    delete x;
+    delete y;
+    delete z;
+    delete mul_x_y;
+    delete res;
 
     return 0;
 }
